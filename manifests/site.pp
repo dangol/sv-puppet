d4fb9a5d356fb8d75d1ef54fdd66c18d496bfac1
Dan <dan@madjic.net>
2017-07-11 11:22:15 -0400

    Add iptables config, fail2ban config and puppetfile

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,3 +3,9 @@ node default {
   include ssh
   include ruby
 }
+class { 'fail2ban':
+  ignoreip => ['127.0.0.1', '10.0.0.1'],
+  bantime => '86400',
+  jails => ['ssh', 'ssh-ddos'],
+  maxretry => '2'
+}
