ed7e6471cfcb900a66ca88f3d1e67911c0967adc
Dan <dan@madjic.net>
2017-07-14 16:44:21 -0400

    apache works now! needs more work

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,6 +4,20 @@ node default {
 #  include ruby
   include apache
 }
+class { 'apache':
+  default_vhost => false,
+}
+file { '//srv/staging_bifma/current/':
+    ensure => 'directory',
+    owner  => 'deploy',
+    group  => 'deploy',
+    mode   => '0775',
+    }
+apache::vhost { 'staging.bifma':
+  port          => '80',
+  docroot       => '/srv/staging_bifma/current/public',
+  }
+
 class { 'fail2ban':
   config_file_template     => "fail2ban/${::lsbdistcodename}/etc/fail2ban/jail.conf.erb",
   config_file_options_hash => {
