da5906f5fa3b3b25eea7fa3dbc334ab7237c20cb
Dan Goldberg <dan@madjic.net>
2017-07-19 14:30:37 +0000

    add nodejs and clamav

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,12 +2,30 @@ node default {
   include user
   include ssh
 #  include ruby
+  include docroots
   include apache
 }
+class { 'apache':
+  default_vhost => false,
+}
+apache::vhost { 'staging.bifma':
+  port          => '80',
+  docroot       => '/srv/staging_bifma/current/public',
+  }
 class { 'fail2ban':
   config_file_template     => "fail2ban/${::lsbdistcodename}/etc/fail2ban/jail.conf.erb",
   config_file_options_hash => {
   'bantime' => '86400',
   'maxretry' => '2'
  }
+}
+class clamav {
+  package { 'clamav':
+    ensure => 'installed',
+  }
+}
+class { '::nodejs':
+  manage_package_repo       => false,
+  nodejs_dev_package_ensure => 'present',
+  npm_package_ensure        => 'present',
 }
