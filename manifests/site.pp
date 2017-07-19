fa3dacfd7566d512e307ac869a23acdf53c7da79
Dan Goldberg <dan@madjic.net>
2017-07-19 19:26:17 +0000

    change nodejs

--- manifests/site.pp
+++ manifests/site.pp
@@ -24,8 +24,4 @@ class clamav {
     ensure => 'installed',
   }
 }
-class { '::nodejs':
-  manage_package_repo       => false,
-  nodejs_dev_package_ensure => 'present',
-  npm_package_ensure        => 'present',
-}
+class { 'nodejs': }
