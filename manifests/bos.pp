8a69412aa4002d058d59e43a8dbf77620c397f73
Dan <dan@madjic.net>
2017-08-02 15:26:26 -0400

    add / update cronjobs for bos / nightly

--- manifests/bos.pp
+++ manifests/bos.pp
@@ -3,20 +3,63 @@ node bos {
   include ssh
   include fail2ban
 }
+#staging_bos
+class crons {
+  cron {'cdr_to_lens': 
+    command => '/srv/staging_cdr/current/script/cdr_to_lens > "/home/deploy/cron/cdr_to_lens_`date +\%Y-\%m-\%d`.log" 2>&1', 
+    user => "deploy",
+    hour => 3,
+    minute => 03,
+    }
+  cron { 'push_sv_rsls':
+    command => '/srv/staging_cdr/current/script/push_sv_rsls >> "/home/deploy/cron/push_sv_rsls.log"',
+    user => "deploy",
+    minute => "*/5", 
+    }
+  cron { 'workoff_delayed_jobs':
+    command => '/srv/staging_cdr/current/script/workoff_delayed_jobs >> "/home/deploy/cron/workoff_delayed_jobs.log"',
+    user => "deploy",
+    minute => "*/5",
+    }
 
-Crons
-   if ENV["DEPLOY_STAGE"] == "staging_cdr"
-      #cron 'load_boms_from_db', command: "/srv/staging_cdr/current/script/load_boms_from_db >> /home/deploy/cron/load_boms_from_db.log", user: "deploy", minute: "*/15"
-      #cron 'partner_invitations', command: '/srv/staging_cdr/current/script/partner_invitations > "/home/deploy/cron/partner_invitations.log"', user: "deploy", minute: "*/15"
-      #cron 'rsl_shares', command: '/srv/staging_cdr/current/script/get_shared_rsls > "/home/deploy/cron/shared_rsls.log"', user: "deploy", minute: "*/15"
-
-    elsif ENV["DEPLOY_STAGE"] == "cdr"
-      cron 'load_boms_from_db', command: "/srv/cdr/current/script/load_boms_from_db >> /home/deploy/cron/load_boms_from_db.log", user: "deploy", minute: "15"
-      cron 'partner_invitations', command: '/srv/cdr/current/script/partner_invitations > "/home/deploy/cron/partner_invitations.log"', user: "deploy", minute: "*/15"
-
-      # user guides
-      cron 'lens_user_guide_others', command: '/srv/cdr/current/script/lens_user_guide_others >> /home/deploy/cron/lens_user_guide_others.log', user: "deploy", hour: 5, minute: 35
-      cron 'lens_user_guide_toy', command: '/srv/cdr/current/script/lens_user_guide_toy >> /home/deploy/cron/lens_user_guide_toy.log', user: "deploy", hour: 5, minute: 40
-      cron 'lens_user_guide_auto', command: '/srv/cdr/current/script/lens_user_guide_auto >> /home/deploy/cron/lens_user_guide_auto.log', user: "deploy", hour: 5, minute: 45
-      cron 'lens_user_guide_packaging', command: '/srv/cdr/current/script/lens_user_guide_packaging >> /home/deploy/cron/lens_user_guide_packaging.log', user: "deploy", hour: 5, minute: 50
-      cron 'ssp_user_guide', command: '/srv/cdr/current/script/ssp_user_guide >> /home/deploy/cron/ssp_user_guide.log', user: "deploy", hour: 3, minute: 33
+# prod_bos
+  cron { 'partner_invitations':
+    command => '/srv/cdr/current/script/partner_invitations > "/home/deploy/cron/partner_invitations.log" 2>&1',
+    user => "deploy",
+    minute => "*/15",
+    }
+  cron { 'load_boms_from_db':
+    command => '/srv/cdr/current/script/load_boms_from_db >> /home/deploy/cron/load_boms_from_db.log 2>&1',
+    user => "deploy",
+    minute => "15",
+    }
+  cron { 'lens_user_guide_others':
+    command => '/srv/cdr/current/script/lens_user_guide_others >> /home/deploy/cron/lens_user_guide_others.log 2>&1',
+    user => "deploy",
+    hour => "5",
+    minute => "35",
+    }
+  cron { 'lens_user_guide_toy':
+    command => '/srv/cdr/current/script/lens_user_guide_toy >> /home/deploy/cron/lens_user_guide_toy.log 2>&1',
+    user => "deploy",
+    hour => "5",
+    minute => "40",
+    }
+  cron { 'lens_user_guide_auto';
+    command => '/srv/cdr/current/script/lens_user_guide_auto >> /home/deploy/cron/lens_user_guide_auto.log 2>&1',
+    user => "deploy",
+    hour => "5",
+    minute => "45",
+    }
+  cron { 'lens_user_guide_packaging':
+     command => '/srv/cdr/current/script/lens_user_guide_packaging >> /home/deploy/cron/lens_user_guide_packaging.log 2>&1',
+     user = "deploy",
+     hour => "5",
+     minute => "50",
+     }
+  cron { 'ssp_user_guide':
+     command => '/srv/cdr/current/script/ssp_user_guide >> /home/deploy/cron/ssp_user_guide.log 2>&1',
+     user = "deploy",
+     hour = 3,
+     minute = 33,
+     }
--- manifests/nightly.pp
+++ manifests/nightly.pp
@@ -3,15 +3,20 @@ node nightly {
   include ssh
   include fail2ban
 }
-
- Crons:
-     if ENV["DEPLOY_STAGE"] == "staging_cdr"
-      cron 'cdr_to_lens', :command => '/srv/staging_cdr/current/script/cdr_to_lens > "/home/deploy/cron/cdr_to_lens_`date +\%Y-\%m-\%d`.log" 2>&1', :user => "deploy", :hour => 3, :minute => 03
-      cron 'push_sv_rsls', :command => '/srv/staging_cdr/current/script/push_sv_rsls >> "/home/deploy/cron/push_sv_rsls.log"', :user => "deploy", :minute => "*/5"
-      cron 'workoff_delayed_jobs', :command => '/srv/staging_cdr/current/script/workoff_delayed_jobs >> "/home/deploy/cron/workoff_delayed_jobs.log"', :user => "deploy", :minute => "*/5"
-
-    elsif ENV["DEPLOY_STAGE"] == "cdr"
-      cron 'cdr_to_lens', :command => '/srv/cdr/current/script/cdr_to_lens > "/home/deploy/cron/cdr_to_lens_`date +\%Y-\%m-\%d`.log" 2>&1', :user => "deploy", :hour => 3, :minute => 03
-      cron 'push_sv_rsls', :command => '/srv/cdr/current/script/push_sv_rsls >> "/home/deploy/cron/push_sv_rsls.log" 2>&1', :user => "deploy", :minute => "45"
-      cron 'workoff_delayed_jobs', :command => '/srv/cdr/current/script/workoff_delayed_jobs >> "/home/deploy/cron/workoff_delayed_jobs.log"', :user => "deploy", :minute => "*/5"
- 
+class crons {
+  cron {'cdr_to_lens': 
+    command => '/srv/staging_cdr/current/script/cdr_to_lens > "/home/deploy/cron/cdr_to_lens_`date +\%Y-\%m-\%d`.log" 2>&1', 
+    user => "deploy",
+    hour => 3,
+    minute => 03,
+    }
+  cron { 'push_sv_rsls':
+    command => '/srv/staging_cdr/current/script/push_sv_rsls >> "/home/deploy/cron/push_sv_rsls.log"',
+    user => "deploy",
+    minute => "*/5", 
+    }
+  cron { 'workoff_delayed_jobs':
+    command => '/srv/staging_cdr/current/script/workoff_delayed_jobs >> "/home/deploy/cron/workoff_delayed_jobs.log"',
+    user => "deploy",
+    minute => "*/5",
+    }
