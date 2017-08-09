1255a951186b8aab313c08938a3fa82000c432b6
Dan <dan@madjic.net>
2017-08-09 10:31:18 -0400

    add remaining docroots

--- modules/docroots/manifests/init.pp
+++ modules/docroots/manifests/init.pp
@@ -1,6 +1,6 @@
 class docroots {
 # create document roots for all web containers, list them below:
- $docroots = [ '/srv/staging_bifma/', '/srv/staging_bifma/current/', ]
+$docroots = [  '/srv/','/srv/staging_bifma/','/srv/staging_bifma/current/','/srv/staging_cdr/','/srv/staging_cdr/current/','/srv/staging_mcf/','/srv/staging_mcf/current/','/srv/staging_rapid_screen/','/srv/staging_rapid_screen/current/','/srv/staging_rsl/','/srv/staging_rsl/current/','/srv/staging_ssp1/','/srv/staging_ssp1/current/','/srv/staging_toy/','/srv/staging_toy/current/','/srv/staging_toy2/','/srv/staging_toy2/current/' ]
 
 file { $docroots:
   ensure => 'directory',
